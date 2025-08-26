c6db05a614b54adab6a8a0273f71ab88787c368a
Placardo <20092387@tafe.wa.edu.au>
2025-08-26 17:36:23 +1000

    GJFIDOJSOGIFD

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int drawnNumbers = 5;
+int[] selectedNumbers = new int[drawnNumbers];
 
+for (int i = 0; i<selectedNumbers.Length; ++1)
+{
+    selectedNumbers[i];
+}
 
