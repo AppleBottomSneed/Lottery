8e33eed11b0541d99acd70b8b977bd7836c94bb8
AppleBottomSneed <20092387@tafe.wa.edu.au>
2025-08-27 13:03:12 +0800

    task 1 (incomplete)

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -3,8 +3,31 @@
 int drawnNumbers = 5;
 int[] selectedNumbers = new int[drawnNumbers];
 
-for (int i = 0; i<selectedNumbers.Length; ++1)
+
+
+// Input range is single digits
+int minNumber = 0;  
+int maxNumber = 9;
+
+
+for (int i = 0; i<selectedNumbers.Length; i++)
 {
-    selectedNumbers[i];
+
+    //User input convert to int
+    while (true)
+    {
+        string userInputString = Console.ReadLine();
+        bool parsedNumber = int.TryParse(userInputString, out convertedNumber);
+        if (convertedNumber >= minNumber && convertedNumber <= maxNumber)
+        {
+            Console.WriteLine($"Please enter {drawnNumbers} numbers between {minNumber} - {maxNumber}. Number {selectedNumbers.Length}:");
+            selectedNumbers[i] = convertedNumber;
+        }
+        else
+        {
+            Console.WriteLine($"Incorrect input, please enter number between {minNumber} and {maxNumber}");
+        }
+    }
 }
+
 
